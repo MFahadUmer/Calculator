12dc89278ab67a4f008cded85c8dd2a83171ce4a
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-12-11 19:42:07 +0500

    Add Linters Configration

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  <div className={"mainSection"}>
-    <h2 className={"title"}>Welcome to Math-magicians Inc</h2>
-    <p className={"message"}>This Website is under devepment. Please Visit again Later</p>
-  </div>, document.getElementById('root')
-)
+  <div className="mainSection">
+    <h2 className="title">Welcome to Math-magicians Inc</h2>
+    <p className="message">This Website is under development. Please visit again soon.</p>
+    <p className="milestone">First Milestone</p>
+  </div>, document.getElementById('root'),
+);
