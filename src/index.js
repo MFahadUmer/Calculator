aa01cf3ade9de238beba4839d6808910f4b6f55b
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-12-11 17:33:08 +0500

    Adds Basic Files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <div className={"mainSection"}>
+    <h2 className={"title"}>Welcome to Math-magicians Inc</h2>
+    <p className={"message"}>This Website is under devepment. Please Visit again Later</p>
+  </div>, document.getElementById('root')
+)
