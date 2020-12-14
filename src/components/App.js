f0bd73948c4d661b489ecaaada9341a82a231364
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-12-14 18:07:24 +0500

    Adds App.js Component

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h2>Hey World</h2>;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './components/App';
 
-ReactDOM.render(
-  <div className="mainSection">
-    <h2 className="title">Welcome to Math-magicians Inc</h2>
-    <p className="message">This Website is under development. Please visit again soon.</p>
-    <p className="milestone">First Milestone</p>
-  </div>, document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
