27cf98c225a07821c2af92630073451d07f128a2
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-12-15 13:21:58 +0500

    Adds Calculator Name Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import CalculatorName from './CalculatorName';
 
 const App = () => (
   <div className="calculator">
+    <CalculatorName />
     <Display />
     <ButtonPanel />
   </div>
---
+++ src/components/CalculatorName.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CalculatorName = () => <h2 className="calculatorName">Math Magicians</h2>;
+
+export default CalculatorName;
