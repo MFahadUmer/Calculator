1c419dcca1124ea929d1872b60928d681649cbce
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2021-01-22 20:50:25 +0500

    Adds Styles to Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
-    <h2>Hey WOrld From Home</h2>
+  <div className="homeContainer">
+    <div>
+      <h3 className="homeWelcome">Welcome to our page!</h3>
+    </div>
+    <div className="homeText">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+      </p>
+    </div>
   </div>
 );
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import App from './App';
-import Quote from './Quote';
-import Home from './Home';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <div className="navbarContainer">
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/calculator" component={App} />
-      <Route path="/quote" component={Quote} />
-    </Switch>
     <div>
-      <h2>Math Magicians</h2>
+      <h2 className="title">Math Magicians</h2>
     </div>
     <div>
-      <Link to="/">Home </Link>
-      <Link to="/calculator">Calculator </Link>
-      <Link to="/quote">Quote of the Day </Link>
+      <Link to="/">
+        <p className="links">Home </p>
+      </Link>
+      <span className="verticalLine">|</span>
+      <Link to="/calculator">
+        <p className="links">Calculator </p>
+      </Link>
+      <span className="verticalLine">|</span>
+      <Link to="/quote">
+        <p className="links">Quote Of The Day </p>
+      </Link>
     </div>
   </div>
 );
